8d236167cea06a32063de97dd9616f76fc6ea6f4
219546769 <alanperez@whsat.org>
2019-04-02 22:39:40 +0000

    added api

--- js/script.js
+++ js/script.js
@@ -4,8 +4,17 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+    var choice = $("#choice").val();
+
+  $.ajax({
+        url: "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q= " + choice,
+        method: "GET",
+        success: function(response) {
+            //$('body').append("<img src=" + response.sprites.front_default + ">");
+            console.log(response);
+        },
+    });
+
+
 });
 
